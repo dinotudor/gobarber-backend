e71e3e23d879eb961e8fb6dc2b1100b3e21be4fd
Dino Tudor <dinotudor@gmail.com>
2019-08-22 00:28:19 +0200

    user update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,8 +19,34 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      console.log(`email  ${email}`);
+      const userExists = await User.findOne({
+        where: { email: req.body.email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      console.log(`oldPassword  ${oldPassword}`);
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 export default new UserController();
--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -18,6 +18,4 @@ export default async (req, res, next) => {
   } catch (err) {
     return res.status(401).json({ error: 'Token invalid' });
   }
-
-  return next();
 };
