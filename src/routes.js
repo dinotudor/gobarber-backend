a1ebf143a3bac6c7dc6fa78b0ae03cd6aad72dd8
Dino Tudor <dinotudor@gmail.com>
2019-08-21 02:10:19 +0200

    test get method

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'hello world' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Dino',
+    email: 'dino@tudor.com',
+    password_hash: '1234',
+  });
+  return res.json(user);
+});
 
 export default routes;
