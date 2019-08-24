dba8b2acb1263e3595c4f5364afa603e187321ac
Dino Tudor <dinotudor@gmail.com>
2019-08-24 16:51:52 +0200

    add URL to avatar

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import { get } from 'http';
 
 class File extends Model {
   static init(sequelize) {
@@ -6,6 +7,12 @@ class File extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3333/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
