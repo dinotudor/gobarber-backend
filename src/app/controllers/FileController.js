c1651695f615c9f3a437a250efd8de6f29cc682d
Dino Tudor <dinotudor@gmail.com>
2019-08-24 12:29:38 +0200

    add file WIP

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,6 +1,15 @@
+import File from '../models/File';
+
 class FileController {
   async store(req, res) {
-    return res.json(req.file);
+    const { orginalname: name, filename: path } = req.file;
+    console.log(`NAME->${name} PATH->${path}`);
+    console.log(`reqFILE-> ${req.file.originalname} ${req.file.path}`);
+    const file = await File.create({
+      name,
+      path,
+    });
+    return res.json(file);
   }
 }
 
