ddb3240511ac0832faee234fc3c290d358cd4858
Dino Tudor <dinotudor@gmail.com>
2019-08-25 13:39:43 +0200

    create notification schema

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,9 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
 import Appointment from '../models/Appointment';
 import File from '../models/File';
 import User from '../models/User';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -83,6 +84,17 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    /* Notify appointemnt provider */
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(hourStart, "'The' dd 'of' MMMM', at' H:mm'h'");
+    console.log(formattedDate, user);
+
+    await Notification.create({
+      content: `New appointment from ${user.name} on ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
